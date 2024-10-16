6e6f21a613ce70dcd6650227645876d24bc34965
yonggyo1125 <yonggyo00@kakao.com>
2024-10-16 18:45:13 +0900

    .

---
+++ 1. 자바/source/day01/src/exam01/Ex01.java
@@ -0,0 +1,12 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        int num; // 변수 선언, 값을 담을 수 있는 공간(메모리) 할당
+        num = 10; // = (대입 연산자), 오른쪽에 있는 10이라는 값을 왼쪽 변수 num 공간에 저장(대입)
+
+        int num2 = 20; // 변수 선언과 동시에 초기화 - 초기화 : 변수에 최초로 값을 저장 하는 것
+
+        System.out.println(num2);
+    }
+}
---
+++ 1. 자바/source/day01/src/exam01/Ex02.java
@@ -0,0 +1,11 @@
+package exam01;
+
+public class Ex02 {
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        int numOfStudent;
+    }
+}
