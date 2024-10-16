cdc061597b3ea9815cb02fa5e477310c5563922b
yonggyo1125 <yonggyo00@kakao.com>
2024-10-16 20:01:09 +0900

    .

---
+++ 1. 자바/source/day02/src/exam01/Ex01.java
@@ -0,0 +1,9 @@
+package exam01;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        byte num1 = 100; // -128 ~ 127
+        short num2 = 10000;
+        int num3 = 10000000;
+    }
+}
---
+++ 1. 자바/source/day02/src/exam01/Ex02.java
@@ -0,0 +1,15 @@
+package exam01;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        char ch1 = 'A';
+        System.out.println(ch1 + 1);
+
+        char ch2 = '가';
+        System.out.println(ch2 + 1);
+
+        System.out.println('A' < 'B');
+
+        System.out.println('가' < '나');
+    }
+}
