0e9844b6647e0383e6a0524e027fcc4748b997d5
yonggyo1125 <yonggyo00@kakao.com>
2024-10-21 19:08:21 +0900

    .

--- 1. 자바/source/day04/src/exam02/Q4.java
+++ 1. 자바/source/day04/src/exam02/Q4.java
@@ -3,6 +3,11 @@ package exam02;
 public class Q4 {
     public static void main(String[] args) {
         // 구구단을 홀수 단만 출력하도록 프로그램을 만드시오.
-
+        for (int i = 2; i <= 9; i++) { // 단
+            System.out.printf("--- %d단 ---%n", i);
+            for (int j = 1; j <= 9; j++) { // 곱하는 수
+                System.out.printf("%d X %d = %d%n", i, j, i * j);
+            }
+        }
     }
 }
