5b0d85d644a872fd1d7e7c808e3c6412ad00d718
yonggyo1125 <yonggyo00@kakao.com>
2024-10-24 19:23:32 +0900

    .

--- 1. 자바/source/day07/src/exam03/Ex01.java
+++ 1. 자바/source/day07/src/exam03/Ex01.java
@@ -3,6 +3,6 @@ package exam03;
 public class Ex01 {
     public static void main(String[] args) {
         Student s1 = new Student();
-
+        s1 = null; // null - 값이 없는 상태 
     }
 }
---
+++ 1. 자바/source/day08/src/exam01/CreditCard.java
@@ -0,0 +1,17 @@
+package exam01;
+
+public class CreditCard {
+    int cardNo; // 신용카드 번호
+    String holder; // 신용카드 소유자명
+    String validDate; // 유효기간
+
+    public CreditCard(int _cardNo, String _holder, String _validDate) {
+        cardNo = _cardNo;
+        holder = _holder;
+        validDate = _validDate;
+    }
+
+    void showInfo() {
+        System.out.printf("cardNo=%d, holder=%s, validDate=%s%n", cardNo, holder, validDate);
+    }
+}
