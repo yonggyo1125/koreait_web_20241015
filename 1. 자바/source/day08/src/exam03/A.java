acef9c6875060c6ac08cf4748e9c32edfb7b75c3
yonggyo1125 <yonggyo00@kakao.com>
2024-10-24 20:28:01 +0900

    .

--- 1. 자바/source/day08/src/exam03/A.java
+++ 1. 자바/source/day08/src/exam03/A.java
@@ -1,10 +1,14 @@
 package exam03;
 
-public class A {
+public class A { // 클래스 내부
     int num1; // default 접근 제어자 - 동일 패키지 내에서만 접근 가능
     protected int num2; // 동일 패키지 내에서 접근 가능(default) + 상속을 통하면 다른 패키지더라도 클래스 내부(private)에서 접근 가능
 
     public int num3; // 동일 패키지, 다른 패키지 모두 접근 가능
 
     private int num4; // 클래스 내부에서만 접근 가능
+
+    void method1() { // 인스턴스 메서드 : 객체가 생성되어야 호출 할 수 있는 메서드
+        System.out.println("메서드1");
+    }
 }
--- 1. 자바/source/day08/src/exam03/Ex01.java
+++ 1. 자바/source/day08/src/exam03/Ex01.java
@@ -2,11 +2,13 @@ package exam03;
 
 public class Ex01 {
     public static void main(String[] args) {
-        A a = new A();
+        A a = new A(); // 클래스 외부
         a.num1 = 10; // default
         System.out.println(a.num1);
 
         a.num2 = 20; // protected
         System.out.println(a.num2);
+
+        //a.num4;  private
     }
 }
