0064780b40ca71bb01e9d9a0bae42adc7b1fb50e
yonggyo1125 <yonggyo00@kakao.com>
2024-10-29 21:19:18 +0900

    .

--- 1. 자바/source/day11/src/exam01/Calculator.java
+++ 1. 자바/source/day11/src/exam01/Calculator.java
@@ -1,5 +1,11 @@
 package exam01;
 
 public abstract class Calculator { // 추상클래스
-     public abstract int add(int num1, int num2); // 추상 메서드
+
+
+     public Calculator() {
+          System.out.println("Calculator 생성자!");
+     }
+
+     public abstract int add(int num1, int num2); // 추상 메서드, 설계 도구
 }
--- 1. 자바/source/day11/src/exam01/SimpleCalculator.java
+++ 1. 자바/source/day11/src/exam01/SimpleCalculator.java
@@ -1,6 +1,11 @@
 package exam01;
 
 public class SimpleCalculator extends Calculator{
+
+    public SimpleCalculator() {
+        super(); // Calculator()
+    }
+
     @Override
     public int add(int num1, int num2) {
 
