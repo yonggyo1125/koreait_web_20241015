6f823ca8918863a8a591c7b47fe335ca1fe6140f
yonggyo1125 <yonggyo00@kakao.com>
2024-10-29 20:51:36 +0900

    .

--- 1. 자바/source/day10/src/exam02/Animal.java
+++ 1. 자바/source/day10/src/exam02/Animal.java
@@ -1,7 +1,11 @@
 package exam02;
 
-public class Animal {
+public abstract class Animal {
+    public abstract void move();
+    /*
     protected void move() {
         System.out.println("움직인다.");
     }
+
+     */
 }
