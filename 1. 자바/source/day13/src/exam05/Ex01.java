1bf6420eadc5654e15a32139a96d9a00d635ec82
yonggyo1125 <yonggyo00@kakao.com>
2024-10-31 21:41:25 +0900

    .

--- 1. 자바/source/day13/src/exam05/Ex01.java
+++ 1. 자바/source/day13/src/exam05/Ex01.java
@@ -2,11 +2,18 @@ package exam05;
 
 public class Ex01 {
     public static void main(String[] args) {
+
         int num1 = 10;
         int num2 = 0;
 
-        int result = num1 / num2; // 예외가 발생하면 프로그램이 중단!
-        System.out.println(result);
+        try {
+            int result = num1 / num2; // 예외가 발생하면 프로그램이 중단!
+            // throw new ArithmeticException();
+            System.out.println(result);
+
+        } catch (ArithmeticException e) {
+            System.out.println("예외 발생!");
+        }
 
         System.out.println("매우 중요한 코드... 실행이 안되면 아주 큰 문제 발생!");
     }
--- 1. 자바/source/day13/src/exam05/Ex02.java
+++ 1. 자바/source/day13/src/exam05/Ex02.java
@@ -1,9 +1,18 @@
 package exam05;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 public class Ex02 {
     public static void main(String[] args) {
-        FileInputStream fis = new FileInputStream("a.txt");
+        try {
+            FileInputStream fis = new FileInputStream("b.txt");
+            // throw new FileNotFoundException();
+
+            System.out.println("문제 없음!");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("예외 발생!");
+        }
     }
 }
