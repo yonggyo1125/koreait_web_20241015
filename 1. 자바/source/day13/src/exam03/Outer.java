b1e18d42a167ca621b117801892cfee5c3cc5114
yonggyo1125 <yonggyo00@kakao.com>
2024-10-31 19:31:01 +0900

    .

--- 1. 자바/source/day13/src/exam03/Outer.java
+++ 1. 자바/source/day13/src/exam03/Outer.java
@@ -1,4 +1,14 @@
 package exam03;
 
 public class Outer {
+    void method() {
+        class Inner {
+            void innerMethod() {
+                System.out.println("메서드!");
+            }
+        }
+
+        Inner in = new Inner();
+        in.innerMethod();
+    }
 }
