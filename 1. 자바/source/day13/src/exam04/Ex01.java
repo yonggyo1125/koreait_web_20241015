907b6ca38249a9fda15d2eab917dde211d47b17b
yonggyo1125 <yonggyo00@kakao.com>
2024-10-31 19:51:28 +0900

    .

--- 1. 자바/source/day13/src/exam04/Ex01.java
+++ 1. 자바/source/day13/src/exam04/Ex01.java
@@ -3,6 +3,6 @@ package exam04;
 public class Ex01 {
     public static void main(String[] args) {
         Outer out = new Outer();
-        out.method();
+        Calculator cal = out.method();
     }
 }
