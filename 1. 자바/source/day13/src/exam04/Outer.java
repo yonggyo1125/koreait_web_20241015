0f77a1a51ce52fef080e5df002179686517a1d1b
yonggyo1125 <yonggyo00@kakao.com>
2024-10-31 19:47:29 +0900

    .

--- 1. 자바/source/day13/src/exam04/Outer.java
+++ 1. 자바/source/day13/src/exam04/Outer.java
@@ -1,14 +1,16 @@
 package exam04;
 
 public class Outer {
-    void method() {
+    public Calculator method() {
         Calculator cal = new Calculator() {
             public int add(int num1, int num2) {
-                return num1 + num1;
+                return num1 + num2;
             }
         };
 
-        int result = cal.add(10, 20);
-        System.out.println(result);
+        return cal;
+
+        //int result = cal.add(10, 20);
+        //System.out.println(result);
     }
 }
