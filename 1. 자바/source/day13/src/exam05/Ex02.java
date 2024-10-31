1d6821c64853e527554efa3f53ce4ac39a10494c
yonggyo1125 <yonggyo00@kakao.com>
2024-10-31 21:13:40 +0900

    .

---
+++ 1. 자바/source/day13/src/exam05/Ex02.java
@@ -0,0 +1,9 @@
+package exam05;
+
+import java.io.FileInputStream;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        FileInputStream fis = new FileInputStream("a.txt");
+    }
+}
