ca0d988a06295f71d3fc582c86ee6e3754de1941
yonggyo1125 <yonggyo00@kakao.com>
2024-11-01 20:28:25 +0900

    .

--- 1. 자바/source/day14/src/exam02/PasswordMismatchException.java
+++ 1. 자바/source/day14/src/exam02/PasswordMismatchException.java
@@ -1,6 +1,6 @@
 package exam02;
 
-public class PasswordMismatchException extends Exception {
+public class PasswordMismatchException extends RuntimeException /*extends Exception */ {
     public PasswordMismatchException(String message) {
         super(message);
     }
--- 1. 자바/source/day14/src/exam02/UserIdNotFoundException.java
+++ 1. 자바/source/day14/src/exam02/UserIdNotFoundException.java
@@ -1,6 +1,6 @@
 package exam02;
 
-public class UserIdNotFoundException extends Exception {
+public class UserIdNotFoundException extends RuntimeException /* extends Exception */ {
     public UserIdNotFoundException(String message) {
         super(message);
     }
