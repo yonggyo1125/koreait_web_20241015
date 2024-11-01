22c183e69b7222721a7ddf42137723fed9f47e51
yonggyo1125 <yonggyo00@kakao.com>
2024-11-01 21:05:15 +0900

    .

--- 1. 자바/source/day14/src/exam03/Ex02.java
+++ 1. 자바/source/day14/src/exam03/Ex02.java
@@ -1,7 +1,18 @@
 package exam03;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Ex02 {
     public static void main(String[] args) {
+        try {
+            FileInputStream fis = new FileInputStream("a.txt");
 
+            fis.close();
+            System.out.println("자원 해제 완료!");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
