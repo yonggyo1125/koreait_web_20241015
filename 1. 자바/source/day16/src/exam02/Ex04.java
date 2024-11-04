a874d3b0afb951616831097c1dd0623f62da2b00
yonggyo1125 <yonggyo00@kakao.com>
2024-11-04 20:09:06 +0900

    ...

--- 1. 자바/source/day16/src/exam02/Ex04.java
+++ 1. 자바/source/day16/src/exam02/Ex04.java
@@ -2,6 +2,11 @@ package exam02;
 
 public class Ex04 {
     public static void main(String[] args) {
-
+        String s1 = new String("ABC");
+        String s2 = new String("ABC");
+        System.out.printf("s1 == s2: %s%n", s1 == s2); // 동일성 비교
+        System.out.printf("s1.equals(s2): %s%n", s1.equals(s2)); // 동등성 비교
+        System.out.printf("s1 주소: %d%n", System.identityHashCode(s1));
+        System.out.printf("s2 주소: %d%n", System.identityHashCode(s2));
     }
 }
