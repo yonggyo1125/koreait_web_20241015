dd31ff5a7f48b40a7655a22e277d69f1ca1ab52c
yonggyo1125 <yonggyo00@kakao.com>
2024-11-05 20:47:30 +0900

    .

---
+++ 1. 자바/source/day17/src/exam04/Ex01.java
@@ -0,0 +1,14 @@
+package exam04;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Box<Apple> appleBox = new Box<Apple>();
+        // 객체 생성시 <Apple> 힌트 : Object -> Apple 형변환
+        // 객체 생성시 지네릭 타입의 자료형이 결정 된다!
+        appleBox.setItem(new Apple());
+
+        //appleBox.setItem(new Banana()); // 타입 안정성 확보
+        Apple apple = appleBox.getItem(); // 형변환의 번거로움 X
+        System.out.println(apple.info());
+    }
+}
---
+++ 1. 자바/source/day17/src/exam04/Ex02.java
@@ -0,0 +1,8 @@
+package exam04;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        Box<Grape> grapeBox = new Box<Grape>();
+        // 객체 생성시 <Grape> 힌트 : Object ->  Grape 형변환
+    }
+}
