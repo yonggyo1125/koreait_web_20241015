7660485f0044348907dfe57602f882c85a965f81
yonggyo1125 <yonggyo00@kakao.com>
2024-11-06 21:04:59 +0900

    .

---
+++ 1. 자바/source/day18/src/exam01/Ex04.java
@@ -0,0 +1,8 @@
+package exam01;
+
+public class Ex04 {
+    public static void main(String[] args) {
+        String trans = "TAXI";
+        Transportation taxi = Enum.valueOf(Transportation.class, trans);
+    }
+}
