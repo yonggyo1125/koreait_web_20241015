75115781351cf42f92500ce65e3e9c9cd64a6aa1
yonggyo1125 <yonggyo00@kakao.com>
2024-11-11 19:50:59 +0900

    .

--- 1. 자바/source/day21/src/exam01/Ex01.java
+++ 1. 자바/source/day21/src/exam01/Ex01.java
@@ -1,4 +1,16 @@
 package exam01;
 
 public class Ex01 {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator() {
+            @Override
+            public int add(int num1, int num2) {
+                return num1 + num2;
+            }
+        };
+
+        Calculator cal2 = (a, b) -> a + b;
+        int result = cal2.add(10, 20);
+        System.out.println(result);
+    }
 }
