ebacaadfab6d4379de48376a2b6acddf7e626afc
yonggyo1125 <yonggyo00@kakao.com>
2024-11-11 20:13:30 +0900

    .

---
+++ 1. 자바/source/day21/src/exam01/Ex02.java
@@ -0,0 +1,25 @@
+package exam01;
+
+import java.util.Arrays;
+import java.util.function.IntPredicate;
+
+public class Ex02 {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        /*
+        IntPredicate conds = new IntPredicate() {
+            @Override
+            public boolean test(int value) {
+                return value % 2 == 1;
+            }
+        };
+        */
+        int sum = Arrays.stream(nums).filter(new IntPredicate() {
+            @Override
+            public boolean test(int value) {
+                return false;
+            }
+        }).sum();
+        System.out.println(sum);
+    }
+}
